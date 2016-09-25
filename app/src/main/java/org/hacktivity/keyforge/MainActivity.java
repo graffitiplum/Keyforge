23d0f18fdc9ab97722a7c7160c43004510b38539
Graffiti Plum <graffitiplum@hacktivity.org>
2016-09-25 16:02:30 -0500

    Ready for deployment.

--- app/src/main/java/org/hacktivity/keyforge/MainActivity.java
+++ app/src/main/java/org/hacktivity/keyforge/MainActivity.java
@@ -6,25 +6,29 @@
 
 package org.hacktivity.keyforge;
 
-import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import org.hacktivity.BlumBlumShub;
+import java.security.*;
+import javax.crypto.*;
+
 import org.hacktivity.Base64;
+import org.hacktivity.BlumBlumShub;
+
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button forgeButton;
     private EditText showPassword;
     private BlumBlumShub bbs;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +38,6 @@ public class MainActivity extends AppCompatActivity {
         forgeButton = (Button) findViewById(R.id.forgebutton);
         showPassword = (EditText) findViewById(R.id.showpassword);
 
-        // TODO: make this happen in the background or display a "loading" page.
-        bbs = new BlumBlumShub(4096);
-
         // Check if no view has focus:
         View view = this.getCurrentFocus();
         if (view != null) {
@@ -44,18 +45,31 @@ public class MainActivity extends AppCompatActivity {
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
 
-
     /*
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
         );
     */
 
+        //progressDialog = ProgressDialog.show(MainActivity.this,"Loading...",
+             //   "Loading application View, please wait...", false, false);
+
+        // TODO: THREADS???
+        bbs = new BlumBlumShub(2310);
+
+        //progressDialog.dismiss();
+
     }
 
-    public void onClickForge(View view) {
-        String pass = Base64.encodeBytes(bbs.randBytes(6));
-        showPassword.setText(pass);
+    public void onClickForge(View view) throws NoSuchAlgorithmException {
+
+        // TODO: organize this.
+        MessageDigest md = MessageDigest.getInstance("SHA-224");;
+
+        //String pass = Base64.encodeBytes(bbs.randBytes(6));
+        md.update(bbs.randBytes(16384));
+
+        showPassword.setText(Base64.encodeBytes(md.digest()).substring(0,8));
     }
 
 }
